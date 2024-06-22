cf3ed1e38554c6b6680fc5889d26091e03166e88
CHUNGSEV <116159227+CHUNGSEV@users.noreply.github.com>
2024-06-22 15:20:14 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,6 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git x https://github.com/CHUNGSEV/X.git' >>feeds.conf.default
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;1.29-1' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
+#echo 'src-git x https://github.com/CHUNGSEV/X.git' >>feeds.conf.default
+#echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;1.29-1' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
